6cbd5fe5782c2630b3acfbcd07f9904f120e61ea
theMstafa <mm22180481@gmail.com>
2022-10-25 04:40:28 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+let employee = {
+    name: "Mustafa",
+  };
+function updateEmployeeWithKeyAndValue(employee, key, value){
+   return { ...employee, ...{ [key]: value } }
+}
+// console.log(updateEmployeeWithKeyAndValue(employee, key, value))
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value ;
+    return employee
+}
+// console.log(destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value))
+function deleteFromEmployeeByKey(employee, key){
+     // delete employee[key];
+
+    // return employee;
+ let newEmployee = { ...employee };
+ delete newEmployee[key]
+ return newEmployee
+   
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+
+    return employee;
+}
